0c10445467e818281d0c0e963c2065b7cbfb8149
ogahozy <tedi242010@gmail.com>
2020-08-05 15:10:35 +0000

    On branch master 	modified:   app.db 	modified:   app/auth/email.py 	modified:   config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER','smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT','465'))
+    MAIL_PORT = int(os.environ.get('MAIL_PORT','587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
